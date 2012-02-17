95ac54aeb1ce07293f082af767b1354456cc2fd7
Iain Stubbs <stubbsi@gmail.com>
2012-02-17 13:57:02 +1100

    added QValidator

---
+++ quickdialog/QValidator.m
@@ -0,0 +1,17 @@
+//
+//  QValidator.m
+//  QuickDialog
+//
+//  Created by Ken Cooper on 2/9/12.
+//  Copyright (c) 2012 Coopercode, LLC. All rights reserved.
+//
+
+#import "QValidator.h"
+
+@implementation QValidator
+-(NSString *)validate: (QElement *)element
+{
+    return nil;
+}
+@end
+
