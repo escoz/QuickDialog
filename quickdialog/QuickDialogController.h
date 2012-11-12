bd5db4db932c38b4ee44a733e9afdd9ea2f62ac2
Michael "Protogenes" Kunz <Michael.Kunz@s2005.tu-chemnitz.de>
2012-11-12 15:17:06 +0100

    + convenience functions to hide and show elements or sections

--- quickdialog/QuickDialogController.h
+++ quickdialog/QuickDialogController.h
@@ -46,4 +46,11 @@
 
 + (UINavigationController *)controllerWithNavigationForRoot:(QRootElement *)root;
 
+
+- (void) hideElementsWithInsertAnimation:(UITableViewRowAnimation)insertAnimation removeAnimation:(UITableViewRowAnimation)removeAnimation elements:(QElement*)element,...;
+- (void) hideSectionsWithInsertAnimation:(UITableViewRowAnimation)insertAnimation removeAnimation:(UITableViewRowAnimation)removeAnimation sections:(QSection*)section,...;
+
+- (void) hideElementsWithAnimation:(UITableViewRowAnimation)animation elements:(QElement*)element,...;
+- (void) hideSectionsWithAnimation:(UITableViewRowAnimation)animation sections:(QSection*)section,...;
+
 @end
