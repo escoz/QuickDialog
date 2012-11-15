d96429390c477a0d853cf70fbda503d3e4702580
Eduardo Scoz <eduardoscoz@gmail.com>
2012-11-15 22:18:33 +0100

    Controllers for root should really always be inside a navigation controller

--- quickdialog/QuickDialogController.m
+++ quickdialog/QuickDialogController.m
@@ -139,7 +139,7 @@
     if (self.navigationController != nil ){
         [self.navigationController pushViewController:newController animated:YES];
     } else {
-        [self presentModalViewController:newController animated:YES];
+        [self presentModalViewController:[[UINavigationController alloc] initWithRootViewController:newController] animated:YES];
     }
 }
 
