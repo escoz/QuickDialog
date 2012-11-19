952f9d2f430982cb2779a498f10a997d179ab698
Eduardo Scoz <eduardoscoz@gmail.com>
2012-11-19 16:00:35 +0100

    Moved methods to a category

--- quickdialog/QuickDialogController.h
+++ quickdialog/QuickDialogController.h
@@ -47,10 +47,4 @@
 + (UINavigationController *)controllerWithNavigationForRoot:(QRootElement *)root;
 
 
-- (void) hideElementsWithInsertAnimation:(UITableViewRowAnimation)insertAnimation removeAnimation:(UITableViewRowAnimation)removeAnimation elements:(QElement*)element,...;
-- (void) hideSectionsWithInsertAnimation:(UITableViewRowAnimation)insertAnimation removeAnimation:(UITableViewRowAnimation)removeAnimation sections:(QSection*)section,...;
-
-- (void) hideElementsWithAnimation:(UITableViewRowAnimation)animation elements:(QElement*)element,...;
-- (void) hideSectionsWithAnimation:(UITableViewRowAnimation)animation sections:(QSection*)section,...;
-
 @end
