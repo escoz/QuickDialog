73c874b16d6ed1fb7a8ec6d33722a9e51e6ea120
Eduardo Scoz <eduardoscoz@gmail.com>
2013-03-28 22:09:31 +0100

    Reverted to the working segmentedelement. The code changes that were accepted never quite worked properly.

--- quickdialog/QSegmentedElement.m
+++ quickdialog/QSegmentedElement.m
@@ -40,15 +40,14 @@
 }
 
 - (void)handleSegmentedControlValueChanged:(id)control {
-    _selected = ((UISegmentedControl *)control).selectedSegmentIndex - 1;
+    _selected = ((UISegmentedControl *)control).selectedSegmentIndex;
     if (self.onValueChanged!=nil)
         self.onValueChanged(self);
 
     [self handleElementSelected:_controller];
 }
 
-/*
- 
+
 - (UITableViewCell *)getCellForTableView:(QuickDialogTableView *)tableView controller:(QuickDialogController *)controller {
     _controller = controller;
     QTableViewCell *cell = [[QTableViewCell alloc] init];
@@ -64,47 +63,6 @@
     control.tag = 4321;
     
     [cell addSubview:control];
-    return cell;
-}
-
-*/
-
-// heavily modified to clip the round borders of the segmented control with extra elements of defined size
-- (UITableViewCell *)getCellForTableView:(QuickDialogTableView *)tableView controller:(QuickDialogController *)controller {
-    _controller = controller;
-    QTableViewCell *cell = [[QTableViewCell alloc] init];
-    cell.backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    cell.backgroundColor = [UIColor clearColor];
-    
-    NSMutableArray * item = [NSMutableArray arrayWithArray:_items];
-    [item addObject:@""];
-    [item insertObject:@"" atIndex:0];
-    CGRect frame = cell.contentView.bounds;
-    frame.origin.x -=1;
-    frame.size.width += 2;
-    frame.size.height += 1;    
-    UIView * container = [[UIView alloc] initWithFrame: frame];
-    container.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    container.clipsToBounds = YES;
-    [cell.contentView addSubview:container];
-    
-    UISegmentedControl *control = [[UISegmentedControl alloc] initWithItems:item];
-    [control addTarget:self action:@selector(handleSegmentedControlValueChanged:) forControlEvents:UIControlEventValueChanged];
-    frame.origin.x -= 9;
-    frame.size.width += 20;
-    control.frame = frame;
-    control.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    control.segmentedControlStyle = 6;
-    control.selectedSegmentIndex = _selected;
-    control.tag = 4321;
-    [control setEnabled:NO forSegmentAtIndex:0];
-    [control setEnabled:NO forSegmentAtIndex:item.count-1];
-    [control setWidth:10 forSegmentAtIndex:0];
-    [control setWidth:10 forSegmentAtIndex:item.count-1];
-
-    control.userInteractionEnabled = self.enabled;
-
-    [container addSubview:control];
     return cell;
 }
 
