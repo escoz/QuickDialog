55038f54c307c2ca6d32994d0eb333f7fa5beadd
Bart Vandendriessche <bart.vandendriessche@gmail.com>
2013-04-29 21:40:49 +0200

    Fix QFloatElement rendering issue with custom QFloatTableViewCell.
    Added a new QFloatTableViewCell with its own layoutSubviews
    implementation to keep a consistent presentation of the QFloatElement
    slider on both iPhone and iPad.

---
+++ QFloatTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  QFloatTableViewCell.h
+//  QuickDialog
+//
+//  Created by Bart Vandendriessche on 29/04/13.
+//
+//
+
+#import "QTableViewCell.h"
+
+@interface QFloatTableViewCell : QTableViewCell
+
+@property (nonatomic, strong, readonly) UISlider *slider;
+
+@end
