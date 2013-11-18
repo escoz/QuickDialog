13aa2baf730928f30d03c19746463dc3890dd8aa
michelevirgilio <michelevirgilio@Micheles-Mac-Pro.local>
2013-11-18 18:01:52 +0100

    Added support to QPickerElement for submit values

--- extras/QPickerElement.h
+++ extras/QPickerElement.h
@@ -11,11 +11,17 @@
 @property (nonatomic, strong) id<QPickerValueParser> valueParser;
 
 @property (nonatomic, strong) NSArray *items;
+@property (nonatomic, strong) NSArray *itemsValues;
 @property (nonatomic, readonly) NSArray *selectedIndexes;
 
+@property (nonatomic, strong) id displayValue;
+@property (nonatomic, strong) id submitValue;
+
 - (QPickerElement *)initWithTitle:(NSString *)title items:(NSArray *)items value:(id)value;
+- (QPickerElement *)initWithTitle:(NSString *)title items:(NSArray *)items itemsValues:(NSArray *) itemsValues value:(id)value;
 
 - (void)reloadAllComponents;
 - (void)reloadComponent:(NSInteger)index;
+
 
 @end
