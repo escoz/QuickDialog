e44000c88418a93badcd18b3b3d7684f7e87eb36
Andrew Yang <evil_eve_live@outlook.com>
2014-09-10 21:31:01 +0800

    Fix: MultilineElment text value  won't update
    Fix: MultilineElment text value  won't update in iPhone.
    
    Issue:
    1, Start the sample with iPhone(device or simulator), go to "Sample Controls" -> "Multiline".
    2, Type some words and back.
    
    Expected:
    The label right to "Multiline" should show some of words typed in the step 2.
    
    Actually:
    There is nothing updated, the label in the right is blank.

--- quickdialog/QMultilineElement.m
+++ quickdialog/QMultilineElement.m
@@ -68,7 +68,7 @@
 	__weak QMultilineTextViewController *weakTextController = textController;
     textController.willDisappearCallback = ^ {
         weakSelf.textValue = weakTextController.textView.text;
-        [[tableView cellForElement:weakSelf] setNeedsDisplay];
+        [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:NO];
         [tableView deselectRowAtIndexPath:indexPath animated:YES];
     };
     [controller displayViewController:textController withPresentationMode:self.presentationMode];
