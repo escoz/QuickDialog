37f56acc522488c42f52352852a54451cd3f48c4
Benjamin Toueg <benjamin@mobeye-app.com>
2014-09-25 17:17:44 +0200

    Revert ExampleViewController

--- sample/ExampleViewController.h
+++ sample/ExampleViewController.h
@@ -15,6 +15,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ExampleViewController : QuickDialogController<QuickDialogEntryElementDelegate>
+@interface ExampleViewController : QuickDialogController
 
 @end
